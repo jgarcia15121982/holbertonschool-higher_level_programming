8e8c54d33023af46ce2c5e964b3706d485b2493f
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-06 02:22:58 +0000

    [FIX] Fixing validations

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        my_tuple = ("None", )
-        return len(my_tuple), my_tuple[0]
+        return (0, None)
     if sentence:
-        my_tuple = (sentence)
-        return len(my_tuple), my_tuple[0]
+        return (len(sentence), sentence[0])
