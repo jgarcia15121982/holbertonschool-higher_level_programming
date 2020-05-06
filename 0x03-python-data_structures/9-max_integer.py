8990bd33bc9fc1eded9e406f21d8aba1cabf028e
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-06 02:52:45 +0000

    [FIX] Validating to 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
+    if my_list is None or len(my_list) is 0:
         return None
     largest = my_list[0]
     for num in my_list:
