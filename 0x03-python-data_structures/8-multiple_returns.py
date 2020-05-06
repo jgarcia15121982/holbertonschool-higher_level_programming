493d1d7a108a5534ffc5cb3b4593d6b2fc22b075
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-06 01:45:37 +0000

    [ADD] More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        my_tuple = ("None", )
+        return len(my_tuple), my_tuple[0]
+    else:
+        my_tuple = (sentence)
+        return len(my_tuple), my_tuple[0]
