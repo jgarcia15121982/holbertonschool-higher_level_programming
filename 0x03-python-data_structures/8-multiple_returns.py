71c9938df2d4aaddf0c3611bbf669d136342ca9f
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-06 02:15:13 +0000

    [FIX] Checking PEP8 style

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), (sentence[0] if len(sentence) >= 1 else None))
+    if sentence == "":
+        my_tuple = ("None", )
+        return len(my_tuple), my_tuple[0]
+    my_tuple = (sentence)
+    return len(my_tuple), my_tuple[0]
