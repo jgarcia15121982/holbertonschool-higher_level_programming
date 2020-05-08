f2ba8367807c819054ddbba848b01e536393f71a
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-08 01:35:10 +0000

    [ADD] Simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for k, v in a_dictionary.items():
+        if k == key:
+            del a_dictionary[key]
+            return(a_dictionary)
+            break
+    return(a_dictionary)
