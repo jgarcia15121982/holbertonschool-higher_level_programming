35c0e2d2a029ca8770a4c99ca7cddf69f59c0e1e
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-11 04:12:19 +0000

    [FIX] pep8 standard

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 10):
     for b in range(a + 1, 10):
         if a != 8:
-            print("{}{}".format(a,b), end=', ')
+            print("{}{}".format(a, b), end=', ')
         else:
             print('{}{}'.format(a, b))
