85f137c2b14293a9d4ac54169356fe427e5e816d
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-24 04:27:24 +0000

    [ADD] Matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,6 +14,14 @@ def matrix_divided(matrix, div):
 
 
     """
+    if type(matrix) not in [int, float]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of the matrix must have the same size")
+    if type(div) not in [int, float]:
+        raise TypeError ("div must be a number")
+    if div == 0
+        raise ZeroDivisionError("division by zero")
     matrix_b = matrix.copy()
     a = []
     b = []
