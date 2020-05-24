a9b459cc26d8385975d81a8b91c31fbc5d503af4
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-24 05:18:50 +0000

    [ADD] Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-
+"""
+Function that prints a name and a 
+last name
+"""
 
 def say_my_name(first_name, last_name=""):
+    """
+    Function that prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): The first parameter
+        last_name (str): The second parameter
+
+    Returns:
+        The quote My name is <first name> <last name>
+
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
