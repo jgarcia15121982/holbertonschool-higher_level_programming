9ee20317f59d0889dc1d6a091728b81ddf6d28d0
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-03 04:53:57 +0000

    [ADD] Square # 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class Square, inherits from class Rectangle"""
+    def __init__(self, size):
+        """Initialize private instance attribute size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Print information about Square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
