a420b496debd5559b03369ffd7d894fa38491d55
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-06 00:40:03 +0000

    [FIX] Refactoring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,9 +18,8 @@ def add_integer(a, b=98):
         The result of the operation
 
     """
-    if type(a) is not int and type(a) is not float:
+    if (a is None or (not isinstance(a, int) and not isinstance(a,float))):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
