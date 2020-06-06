cb061ba575cf2d568b7c01d51d253dc5d0043e77
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-06 01:33:31 +0000

    [FIX] Adding comments and space

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
+"""
+4-print_square.py file
+Functions:
+-> print_square(size)
+
+"""
 
 
 def print_square(size):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        elif type(size) is float and size < 0:
-            raise TypeError("size must be an integer")
-        else:
-            for i in range(size):
-                for j in range(size):
-                    print("#", end="")
-                print()
+    """
+    Function that prints a square with the character #.
+
+    Args:
+        a (int, float): The size of the square
+
+    Returns:
+        Nothing. The function prints the square
+
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
