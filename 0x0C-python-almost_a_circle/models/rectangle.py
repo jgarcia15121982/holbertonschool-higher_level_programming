1c32e2a025cb4c46ea31ba478c143a270f7ca178
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-08 21:34:59 +0000

    [UPDATE] method update() added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,28 @@ class Rectangle(Base):
                                                        self.__y,
                                                        self.__width,
                                                        self.__height)
+    
+    def update(self, *args):
+        """method that update
+            values of the class
+        """
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.id == args[0]
+            self.__width = args[1]
+        if len(args) == 3:
+            self.id == args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+        if len(args) == 4:
+            self.id == args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+        if len(args) == 5:
+            self.id == args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
