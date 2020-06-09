24cf8eb824a78cdd301862d6f1ae428154dd9b5b
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-09 18:36:03 +0000

    [UPDATE] Dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,8 @@ class Square(Rectangle):
     """class Square inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialize Square class"""
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
@@ -32,4 +32,44 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
                                                  self.y,
-                                                 self.width)
+                                                 self.size)
+
+    def update(self, *args, **kwargs):
+        """method that update
+            values of the class
+        """
+        if len(args) != 0:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.id = args[0]
+                self.size = args[1]
+            if len(args) == 3:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+            if len(args) == 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+        else:
+            if kwargs.get('id'):
+                self.id = kwargs['id']
+            if kwargs.get('size'):
+                self.__size = kwargs['size']
+            if kwargs.get('x'):
+                self.__x = kwargs['x']
+            if kwargs.get('y'):
+                self.__y = kwargs['y']
+
+    def to_dictionary(self):
+        """Public method that returns the dictionary
+            representation of a Square
+        """
+        my_dict = {}
+        my_dict['id'] = self.id
+        my_dict['size'] = self.size
+        my_dict['x'] = self.x
+        my_dict['y'] = self.y
+        return my_dict
