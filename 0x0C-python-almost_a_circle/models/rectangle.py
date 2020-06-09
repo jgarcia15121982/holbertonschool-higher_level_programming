96b8b0907644861d1fea865cdbdec2496e575c8a
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-09 03:11:56 +0000

    [UPDATE] Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,27 +94,17 @@ class Rectangle(Base):
                                                        self.__width,
                                                        self.__height)
     
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """method that update
             values of the class
         """
-        if len(args) == 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.id == args[0]
-            self.__width = args[1]
-        if len(args) == 3:
-            self.id == args[0]
-            self.__width = args[1]
-            self.__height = args[2]
-        if len(args) == 4:
-            self.id == args[0]
-            self.__width = args[1]
-            self.__height = args[2]
-            self.__x = args[3]
-        if len(args) == 5:
-            self.id == args[0]
-            self.__width = args[1]
-            self.__height = args[2]
-            self.__x = args[3]
-            self.__y = args[4]
+        if kwargs.get('id'):
+            self.id = kwargs['id']
+        if kwargs.get('width'):
+            self.__width = kwargs['width']
+        if kwargs.get('height'):
+            self.__height = kwargs['height']
+        if kwargs.get('x'):
+            self.__x = kwargs['x']
+        if kwargs.get('y'):
+            self.__y = kwargs['y']
