4f34327779f6d716431a5402e7f02180473d7721
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-10 04:27:12 +0000

    [FIX] setter and getter validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,21 +9,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initialize Square class"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
         """get value of size"""
-        return self.__size
+        return self.height
 
     @size.setter
     def size(self, size):
         """set value of size"""
-        if type(size) is not int:
-            raise TypeError("width must be an integer")
-        if size <= 0:
-            raise ValueError("width must be > 0")
-        self.__size = size
+        self.width = size
+        self.height = size
 
     def __str__(self):
         """method __str__ that returns
@@ -32,13 +28,13 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
                                                  self.y,
-                                                 self.size)
+                                                 self.height)
 
     def update(self, *args, **kwargs):
         """method that update
             values of the class
         """
-        if args and len(args) != 0:
+        if args and len(args) > 0:
             if len(args) == 1:
                 self.id = args[0]
             if len(args) == 2:
@@ -57,11 +53,11 @@ class Square(Rectangle):
             if kwargs.get('id'):
                 self.id = kwargs['id']
             if kwargs.get('size'):
-                self.__size = kwargs['size']
+                self.size = kwargs['size']
             if kwargs.get('x'):
-                self.__x = kwargs['x']
+                self.x = kwargs['x']
             if kwargs.get('y'):
-                self.__y = kwargs['y']
+                self.y = kwargs['y']
 
     def to_dictionary(self):
         """Public method that returns the dictionary
@@ -69,7 +65,7 @@ class Square(Rectangle):
         """
         my_dict = {}
         my_dict['id'] = self.id
-        my_dict['size'] = self.size
+        my_dict['size'] = self.height
         my_dict['x'] = self.x
         my_dict['y'] = self.y
         return my_dict
