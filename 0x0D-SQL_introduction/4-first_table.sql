5bc306896bee03298bcd29be1225e5c8a8b7bf51
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-13 18:43:43 -0500

    [FIX] Correcting script

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Script that creates a table called "first_table"
 -- in the "hbtn_0c_0" database in my MySQL server
- CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
