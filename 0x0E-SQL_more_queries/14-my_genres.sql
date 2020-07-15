0e554ac13e7ade22bf6fea98237d0c04664d55d1
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-14 23:00:06 -0500

    [ADD] My Genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- My genres
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE show_id = (SELECT id
+      	      	 FROM tv_shows 
+		 WHERE title = 'Dexter')
+ORDER BY tv_genres.name ASC;
