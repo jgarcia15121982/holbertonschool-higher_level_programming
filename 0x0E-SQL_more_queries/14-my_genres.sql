aef3ccb2afe9956ae8e40952c4cc0f797f88f613
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-14 23:02:01 -0500

    [FIX] Just one SELECT

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,5 @@ SELECT tv_genres.name
 FROM tv_show_genres
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE show_id = (SELECT id
-      	      	 FROM tv_shows 
-		 WHERE title = 'Dexter')
+WHERE show_id = 8
 ORDER BY tv_genres.name ASC;
