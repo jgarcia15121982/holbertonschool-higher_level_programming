7a1f90d28fb08c82b82eecb33d96d0b94e871184
jgarcia15121982 <johfregarcia@gmail.com>
2020-08-18 20:07:52 -0500

    [UPDATE] adding -s flag

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -Ls "$1" -X DELETE
+curl -sL "$1" -X DELETE
 
 
