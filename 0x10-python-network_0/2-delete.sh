83a68098d19241f276d1411bfd277b9c4c89c77c
jgarcia15121982 <johfregarcia@gmail.com>
2020-08-18 20:06:09 -0500

    [UPDATE] sending a new file

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
+curl -Ls "$1" -X DELETE
+
 
