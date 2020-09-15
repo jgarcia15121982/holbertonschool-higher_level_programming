6dca4795b20a7aaa662f623d2cec5255f4824fbb
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-15 16:11:27 -0500

    [ADD] Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv.length > 2) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
+if (process.argv.length === 2) {
+  console.log('undefined is undefined');
+}
