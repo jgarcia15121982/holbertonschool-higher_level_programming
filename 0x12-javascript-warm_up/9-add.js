f810664dd43ac30e6dde9f80a31fbb978e552a77
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-15 22:01:46 -0500

    [FIX] Fixing program bugs

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,7 +6,5 @@ function add (a, b) {
 if (!process.argv[3] || isNaN(process.argv[2]) || isNaN(process.argv[3])) {
   console.log('Nan');
 } else {
-  const a = parseInt(process.argv[2]);
-  const b = parseInt(process.argv[3]);
-  console.log(add(a, b));
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
 }
