43ceebfb21d677252d15e1086d757e0a79de64a6
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-22 14:29:02 -0500

    [FIX] Fixing writeme.js file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) throw err;
 });
